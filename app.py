dab08d5cbfd5279e9deb2473b7f320d6f0284c19
deemarc <deemarc.br@gmail.com>
2021-02-04 17:28:33 +0700

    small config edit

--- app.py
+++ app.py
@@ -13,12 +13,10 @@ def create_app(config_class=Config):
     app.register_blueprint(checkout)
     app.register_blueprint(webhook)
 
+    # @app.route('/')
+    # def hello_world():
+    #     return 'Hello, World!'
     # Adding Content Security Policy to load all content either locally or from omise.co.
-    Talisman(
-        app,
-        content_security_policy={
-            "default-src": "'unsafe-inline' 'self' *.amazonaws.com *.omise.co"
-        },
-    )
+    Talisman(app)
 
     return app
